fffe21a7ebaf21205db29a6b35598ed211bfa5d4
sherelynn <sherelynnb@missionreadyhq.com>
2025-06-10 16:13:20 +1200

    Initial commit

---
+++ z-energy-frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+
+/*========== Component Imports ==========*/
+import Homepage from "./pages/Home/Homepage"
+import PriceComparison from "./pages/PriceComparison/PriceComparison"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="price-comparison" element={<PriceComparison />} />
+
+        {/* For future nested routes */}
+        {/* <Route path="/nested-route-path" element={<PageName />}>
+          <Route path="nested-route-name1" element={<NestedComponent1 />} />
+          <Route path="nested-route-name2" element={<NestedComponent2 />} />
+        </Route> */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
